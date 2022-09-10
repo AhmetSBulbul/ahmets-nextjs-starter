1401a4df2b8ec4e90979f4d055a7f1d50c8bdc23
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-09-10 10:49:49 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      monts: ["Montserrat", "sans-serif"],
+      rubik: ["Rubik", "sans-serif"],
+    },
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
