6fd610a6e5b8aa89c4149488155f4b78b24c5be9
AhmetSBulbul <ahmetsafabulbul@gmail.com>
2022-09-10 15:12:41 +0300

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,18 @@ import { Player, Controls } from '@lottiefiles/react-lottie-player'
 
 const Home: NextPage = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto h-screen relative'>
       <Navbar />
-      <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-        <h3>Test</h3>
+      <div className='flex flex-col items-center justify-center py-2'>
         <Player
-          src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
-          style={{ height: '300px', width: '300px' }}
-        >
+          autoplay
+          loop
+          src="https://assets9.lottiefiles.com/packages/lf20_7psw7qge.json"
+          className='my-auto'
+        ><Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
         </Player>
       </div>
+      <div className='absolute bottom-0 w-full left-0 flex items-center justify-center py-4'>Developed by github.com/AhmetSBulbul</div>
     </div>
   )
 }
